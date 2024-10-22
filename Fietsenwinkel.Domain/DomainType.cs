78afd180c04f65308477a78aa106541f267032bd
Geerten Vink <geerten@pitcrew.nl>
2024-10-22 16:20:03 +0200

    Added domain type interface

---
+++ Fietsenwinkel.Domain/DomainType.cs
@@ -0,0 +1,9 @@
+﻿namespace Fietsenwinkel.Domain;
+public interface IDomainType<TValueType, TImplementationType> where TImplementationType : IDomainType<TValueType, TImplementationType>
+{
+    public TValueType Value { get; }
+
+    public abstract static bool IsValidDomainTypeFor(TValueType value);
+
+    public abstract static bool TryParse(string value, out TImplementationType? domainType);
+}
