3b26a74bfe1131d12b9b00fa611220baf2aaa371
Geerten Vink <maedaros@yahoo.com>
2024-10-31 12:25:40 +0100

    Small rework of VoorraadDetailsMapper

--- Fietsenwinkel.Database/Mappers/VoorraadDetailsMapper.cs
+++ Fietsenwinkel.Database/Mappers/VoorraadDetailsMapper.cs
@@ -10,29 +10,14 @@ namespace Fietsenwinkel.Database.Mappers;
 
 internal static class VoorraadDetailsMapper
 {
-    public static Result<VoorraadDetails, ErrorCodeList> Map(FiliaalId filiaalId, FietsModel[] fietsen)
-    {
-        ErrorCodeList errors = [];
-
-        Fiets[] mappedFietsen = [];
+    public static Result<VoorraadDetails, ErrorCodeList> Map(FiliaalId filiaalId, FietsModel[] fietsen) =>
         MapFietsen(fietsen).Switch(
-            onSuccess: f => mappedFietsen = f,
-            onFailure: errors.AddRange);
-
-        if (errors.Count > 0)
-        {
-            return Result<VoorraadDetails, ErrorCodeList>.Fail(errors);
-        }
-
-        var voorraadDetails = new VoorraadDetails(filiaalId, mappedFietsen);
-
-        return Result<VoorraadDetails, ErrorCodeList>.Succeed(voorraadDetails);
-    }
+            onSuccess: mappedFietsen => Result<VoorraadDetails, ErrorCodeList>.Succeed(new(filiaalId, mappedFietsen)),
+            onFailure: Result<VoorraadDetails, ErrorCodeList>.Fail);
 
     private static Result<Fiets[], ErrorCodeList> MapFietsen(FietsModel[] fietsen)
     {
         ErrorCodeList errors = [];
-
         List<Fiets> mappedFietsen = [];
 
         foreach (var fiets in fietsen)
